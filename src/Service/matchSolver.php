dedd49c40d5a7c72479830f885fa13739432d0a9
MikkPtr <cacahouettetbs@gmail.com>
2024-12-11 15:32:43 +0100

    fix: fuctionify calls to getters and setters

--- src/Service/matchSolver.php
+++ src/Service/matchSolver.php
@@ -2,28 +2,28 @@
 namespace App\Service;
 
 function matchSolver($player1, $player2, $iteration) {
-    if ($player1->getCurrentAttack == true && $player2->getCurrentAttack == true) {
-        $player1->SetScore += 3;
-        $player2->SetScore += 3;
+    if ($player1->getCurrentAttack() == true && $player2->getCurrentAttack() == true) {
+        $player1->SetScore() += 3;
+        $player2->SetScore() += 3;
     } 
-    else if ($player1->getCurrentAttack == false && $player2->getCurrentAttack == false) {
-        $player1->SetScore += 1;
-        $player2->SetScore += 1;
+    else if ($player1->getCurrentAttack() == false && $player2->getCurrentAttack() == false) {
+        $player1->SetScore() += 1;
+        $player2->SetScore() += 1;
     } 
     else {
-        if ( $player1->getCurrentAttack == false ) {
-            $player1->SetScore += 5;
+        if ( $player1->getCurrentAttack() == false ) {
+            $player1->SetScore() += 5;
         } else {
-            $player2->SetScore += 5;
+            $player2->SetScore() += 5;
         }
     }
 
     $player1->push(array("turn"=>$iteration ,
-                         "choice"=>$player1->getCurrentAttack,
-                         "opponentchoice"=>$player2->getCurrentAttack,
-                         "score"=>$player1->score ));
+                         "choice"=>$player1->getCurrentAttack(),
+                         "opponentchoice"=>$player2->getCurrentAttack(),
+                         "score"=>$player1->SetScore() ));
     $player2->push(array("turn"=>$iteration ,
-                         "choice"=>$player2->getCurrentAttack,
-                         "opponentchoice"=>$player1->getCurrentAttack,
-                         "score"=>$player2->score ));
+                         "choice"=>$player2->getCurrentAttack(),
+                         "opponentchoice"=>$player1->getCurrentAttack(),
+                         "score"=>$player2->SetScore() ));
 }
