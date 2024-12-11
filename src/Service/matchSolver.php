de5d3b70309f5b291468374c21f8af8d6d9b2253
MikkPtr <cacahouettetbs@gmail.com>
2024-12-11 15:33:57 +0100

    fix: defunctionify the setters call

--- src/Service/matchSolver.php
+++ src/Service/matchSolver.php
@@ -3,27 +3,27 @@ namespace App\Service;
 
 function matchSolver($player1, $player2, $iteration) {
     if ($player1->getCurrentAttack() == true && $player2->getCurrentAttack() == true) {
-        $player1->SetScore() += 3;
-        $player2->SetScore() += 3;
+        $player1->SetScore += 3;
+        $player2->SetScore += 3;
     } 
     else if ($player1->getCurrentAttack() == false && $player2->getCurrentAttack() == false) {
-        $player1->SetScore() += 1;
-        $player2->SetScore() += 1;
+        $player1->SetScore += 1;
+        $player2->SetScore += 1;
     } 
     else {
         if ( $player1->getCurrentAttack() == false ) {
-            $player1->SetScore() += 5;
+            $player1->SetScore += 5;
         } else {
-            $player2->SetScore() += 5;
+            $player2->SetScore += 5;
         }
     }
 
     $player1->push(array("turn"=>$iteration ,
                          "choice"=>$player1->getCurrentAttack(),
                          "opponentchoice"=>$player2->getCurrentAttack(),
-                         "score"=>$player1->SetScore() ));
+                         "score"=>$player1->GetScore() ));
     $player2->push(array("turn"=>$iteration ,
-                         "choice"=>$player2->getCurrentAttack(),
+                         "choice"=>$player2->getCurrentAttack,
                          "opponentchoice"=>$player1->getCurrentAttack(),
-                         "score"=>$player2->SetScore() ));
+                         "score"=>$player2->GetScore() ));
 }
